32fa5f0adf773efd381f7a2eb6d3676eafb63d1e
alanxurox <alanxu2013@gmail.com>
2019-11-03 17:11:46 -0500

    Change: AutoCompleteTextView now shows classes with the right period

--- app/src/main/java/org/trinitypawling/tpeoptest/SettingsActivity.java
+++ app/src/main/java/org/trinitypawling/tpeoptest/SettingsActivity.java
@@ -4,30 +4,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.LinearLayout;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    final String[] COURSES = Course.getNameList();
+public class SettingsActivity extends AppCompatActivity {
+    boolean bWeek;
+    //final String[] COURSES = Course.getNameList();
+    ArrayList<Period> schedule = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        LinearLayout vl = findViewById(R.id.vl);
-        final int childCount = vl.getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            if (vl.getChildAt(i).getId() != R.id.space) {
-                AutoCompleteTextView editText = (AutoCompleteTextView) vl.getChildAt(i);
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                        android.R.layout.simple_list_item_1, COURSES);
-                editText.setAdapter(adapter);
-            }
-
+        for (int j = 1; j <= 7; j++) {
+            String viewID = "tv" + j;
+            int resID = getResources().getIdentifier(viewID, "id", getPackageName());
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1, Course.getNameList(j));
+            AutoCompleteTextView editText = findViewById(resID);
+            editText.setAdapter(adapter);
         }
+
     }
 
 
@@ -39,19 +39,25 @@ public class SettingsActivity extends AppCompatActivity {
             Period.loadPeriodsB();
         else
             Period.loadPeriodsA();
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         final Switch aSwitch = findViewById(R.id.switchB);
+
         aSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (aSwitch.isChecked())
+                if (aSwitch.isChecked()) {
                     Period.loadPeriodsB();
-                else
+                    bWeek = true;
+                } else {
                     Period.loadPeriodsA();
+                    bWeek = false;
+                }
             }
         });
     }
