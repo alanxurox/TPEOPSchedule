4fe3702495fe8833fc0cd36ac8f91d6c196db607
Alan Xu <alanxu2013@gmail.com>
2019-11-23 17:40:44 -0500

    edit comment

--- app/src/main/java/org/trinitypawling/tpeoptest/FireBaseCallback.java
+++ app/src/main/java/org/trinitypawling/tpeoptest/FireBaseCallback.java
@@ -5,7 +5,8 @@ import java.util.List;
 /**
  * This interface is designed for loading periods only after firebase data is retrieved, and after that
  * MyDrawable could use the periods array list to draw the schedule, avoiding a nullpointer exception.
- @method onCallBack
+ @method onCallBack is designed to hold the draw functions so that only after onCallBack is called would         
+ * they run, not before the periods are loaded. 
  */
 public interface FireBaseCallback {
     void onCallBack(List<?> list);
